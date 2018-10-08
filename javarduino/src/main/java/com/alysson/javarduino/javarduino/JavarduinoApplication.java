86d4077c1809d4e3d05aabc316fe87b03928bebd
AlyssonRhuan <alysson_salgado@hotmail.com>
2018-10-08 10:48:41 -0300

    Projeto criado e srpingboot testado

---
+++ javarduino/src/main/java/com/alysson/javarduino/javarduino/JavarduinoApplication.java
@@ -0,0 +1,12 @@
+package com.alysson.javarduino.javarduino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarduinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarduinoApplication.class, args);
+	}
+}
---
+++ javarduino/src/test/java/com/alysson/javarduino/javarduino/JavarduinoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alysson.javarduino.javarduino;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavarduinoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
